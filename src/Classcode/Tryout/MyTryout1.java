d276e15c2e24d930fcbacdffc427d4ddbdc46003
Will <will.b.yip@gmail.com>
2021-02-22 22:44:08 -0500

    seeing if can add the new file Tryout

---
+++ src/Classcode/Tryout/MyTryout1.java
@@ -0,0 +1,7 @@
+package Classcode.Tryout;
+
+public class MyTryout1 {
+    public static void main(String[] args) {
+        String problem = "why is the title red, because it has not been uploaded to gitHub";
+    }
+}
