f90c0161c0a5efc36b4535f4f810e78915191f40
Will <will.b.yip@gmail.com>
2021-04-05 17:05:47 -0400

    Hw11 Printer method commands

---
+++ src/Classcode/Class12/PrinterCommands.java
@@ -0,0 +1,60 @@
+package Classcode.Class12;
+
+public class PrinterCommands {
+
+    private int maxToner =100;
+    private int maxPaper =100;
+    int pageInTray = 100;
+    int tonerInTray = 100;
+
+    public void printFile (int printNumber) { //no int after void BC no return is needed with void
+        if (pageInTray-printNumber>0 && tonerInTray-printNumber>0) {
+            pageInTray=pageInTray-printNumber;
+            tonerInTray=tonerInTray-printNumber;
+        } else {
+            System.out.println("Not enough paper or toner");
+        }
+        System.out.println("Single print successful. The number of pages in your tray is: " + pageInTray + ", toner in tray is: " + tonerInTray);
+
+    }
+
+    public void printFile (int printNumber, String doublePage) {
+        int printDoubleNumberAdjust=0;
+        if((printNumber/2)%1!=0) {
+            printDoubleNumberAdjust=printNumber/2+1;
+        } else {
+            printDoubleNumberAdjust=printNumber/2;
+        }
+
+        if (pageInTray-printNumber>0 && tonerInTray-printNumber>0) {
+            pageInTray=pageInTray-(printDoubleNumberAdjust);
+            tonerInTray=tonerInTray-printNumber;
+        } else {
+        System.out.println("Not enough paper or toner");
+    }
+        System.out.println("Double print successful. The number of pages in your tray is: " + pageInTray + ", toner in tray is: " + tonerInTray);
+
+    }
+
+    public void addPaper(int paperAddAmount) {
+        if (pageInTray+paperAddAmount<maxPaper) {
+            pageInTray=pageInTray+paperAddAmount;
+        } else {
+            System.out.println("Your paper adding request has exceeded the max amount");
+        }
+    }
+
+    public void addToner(int TonerAddAmount) {
+        if (tonerInTray+TonerAddAmount<maxToner) {
+            tonerInTray=tonerInTray+TonerAddAmount;
+        } else {
+            System.out.println("Your toner adding request has exceeded the max amount");
+        }
+    }
+
+    public void printerSummary() {
+        System.out.println("Summary: Current page level is: " + pageInTray + ", current level for toner is: " + tonerInTray);
+    }
+
+
+}
---
+++ src/Classcode/Class12/WillYipHw11Printer.java
@@ -0,0 +1,102 @@
+package Classcode.Class12;
+
+public class WillYipHw11Printer {
+    public static void main(String[] args) {
+        /**
+         * Create a Printer
+         *
+         * maxToner level = 100
+         * maxPaper in tray = 100
+         * with 1 page printing, 1 toner is being used **/
+        PrinterCommands p1 = new PrinterCommands();
+        //P1.printFile(5);
+
+
+        /** print a files which is having 5 pages, print single sided
+         * paperInTray = 95
+         * toner = 95 **/
+
+
+        /** print a files which is having 5 pages; print double sided
+         *      paper-1     pg1,2
+         *      paper-2     pg3,4
+         *      paper-3     pg5
+         *  paper used = 3
+         *  toner used = 5
+         *
+         *  paperInTray = 92
+         *  toner = 90 **/
+        //p1.printFile(5,"double");
+
+
+        /**  1. Create method 'print' which can be used to print single-sided or double-sided
+         *      // If paper or toner is not enough to print, we display error msg saying, Not enough paper or toner
+         * 2. Create method 'addPaper' to add papers in the tray
+         * 3. Create method 'addToner' to add toner
+         * 4. Create method 'printerSummary'
+         *          Toner level: YY
+         *          Paper in tray: XX
+         */
+
+
+
+        /**
+         * Create object of Printer class.
+         * Printer p1 = new Printer();
+         *
+         * p1.printerSummary();
+         *          Toner level: 100
+         *          Paper in tray: 100 **/
+         p1.printerSummary();
+
+         /** p1.print(10);        // single sided
+         *
+         * p1.printerSummary();
+         *          Toner level: 90
+         *          Paper in tray: 90 **/
+         p1.printFile(10);
+
+
+         /** p1.print(10, true);  // double sided **/
+        p1.printFile(10,"double");
+
+        /** p1.printerSummary();
+         *          Toner level: 80
+         *          Paper in tray: 85
+         *
+         * p1.print(9, true); **/
+        p1.printFile(9,"doublePage");
+
+
+         /** p1.printerSummary();
+         *          Toner level: 71
+         *          Paper in tray: 80 **/
+         p1.printerSummary();
+
+         /** p1.print(90);
+         *     // If paper or toner is not enough to print, we display error msg saying, Not enough paper or toner **/
+
+         p1.printFile(90);
+
+        /** p1.printerSummary();
+         *          Toner level: 61
+         *          Paper in tray: 70
+         *
+         *
+         * addToner(10)
+         *    Toner level: 71 **/
+         p1.addToner(10);
+
+         /** addToner(30)
+         *      // Invalid toner value **/
+         p1.addToner(30);
+         /** addToner(-10)
+         *      // Invalid toner value **/
+
+
+    }
+
+
+
+
+}
